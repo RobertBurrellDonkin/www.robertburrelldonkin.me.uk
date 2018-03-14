2c3eaf009063f8c21269a02d3621c2f3dd76fd9f
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2018-03-14 09:55:04 +0000

    Trial modification to test whether automatic update works.

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default () => <div style={{ margin: '10%', textAlign: 'center'}}>
     <h1>Coming Soon</h1>
-    <p>A wonderful new website!</p>
+    <p>My wonderful new website!</p>
     <p>Please check back again soon.</p>
 </div>
